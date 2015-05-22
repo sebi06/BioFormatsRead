a1a0d7db3a6726a573b0edddc500add936301a53
sebi06 <sebrhode@gmail.com>
2015-05-22 17:20:05 +0200

    Create __init__.py

---
+++ bfimage/__init__.py
@@ -0,0 +1,13 @@
+"""
+@author: Sebastian Rhode
+
+File: __init__.py
+Version: 0.1
+Date: 18.05.2015
+"""
+
+__author__ = 'M1SRH'
+
+from czitools import *
+from bftools import *
+from misctools import *
