72aa045a2a3428c935f5d9a7d2e66412a6af7050
sebi06 <sebrhode@gmail.com>
2016-04-03 09:49:38 +0200

    Updates.

--- bftest.py
+++ bftest.py
@@ -12,23 +12,6 @@ import os
 import bfimage as bf
 
 
-def func(filename):
-
-    imgdir = os.path.dirname(filename)
-    imgbase = os.path.basename(filename)
-
-    return imgbase, imgdir
-
-def test_basic_filenames():
-
-    filename = r'testdata/Beads_63X_NA1.35_xy=0.042_z=0.1.czi'
-
-    ib, id = func(filename)
-    print '!!!'
-    assert ib == os.path.basename(filename)
-    assert id == os.path.dirname(filename)
-
-
 def test_metainfo():
 
     filename = r'testdata/Beads_63X_NA1.35_xy=0.042_z=0.1.czi'
