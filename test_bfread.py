4183cce5e4f133ed3b7a924a89a490d3a58c4b08
sebi06 <sebi06@users.noreply.github.com>
2016-04-08 15:15:39 +0200

    Update test_bfread.py
    Updates

--- test_bfread.py
+++ test_bfread.py
@@ -2,9 +2,9 @@
 """
 @author: Sebi
 
-File: test_get_image6d.py
-Date: 03.02.2016
-Version. 1.0
+File: test_bfread.py
+Date: 08.04.2016
+Version. 0.5
 """
 
 import numpy as np
@@ -57,6 +57,58 @@ def test_metainfo():
     dims = [1, 1, 100, 1, 346, 580]
     for i in range(0, len(dims)):
         assert np.shape(img6d)[i] == dims[i]
+
+
+def test_timeseries():
+
+    filename = r'testdata/T=5_Z=3_CH=2_CZT_All_CH_per_Slice.czi'
+    # get image meta-information
+    MetaInfo = bf.bftools.get_relevant_metainfo_wrapper(filename)
+    seriesID = 0
+    timepoint = 2
+    channel = 1
+    zplane = 2
+    dims = [5, 2, 640, 640]
+    # get the actual time series from the data set
+    tseries = bf.bftools.get_timeseries(filename, MetaInfo['Sizes'], seriesID, zplane)
+
+    for i in range(0, len(dims)):
+        assert np.shape(tseries)[i] == dims[i]
+
+
+def test_zstack():
+
+    filename = r'testdata/Beads_63X_NA1.35_xy=0.042_z=0.1.czi'
+    # get image meta-information
+    MetaInfo = bf.bftools.get_relevant_metainfo_wrapper(filename)
+    seriesID = 0
+    timepoint = 0
+    channel = 0
+    dims = [100, 1, 346, 580]
+    # get the actual z-stack from the data set
+    zstack = bf.bftools.get_zstack(filename, MetaInfo['Sizes'], seriesID, timepoint)
+
+    # get plane with the brightest pixel
+    zplane = (zstack == zstack.max()).nonzero()[0][0]
+    # check found zplane
+    assert zplane == 46
+
+    for i in range(0, len(dims)):
+        assert np.shape(zstack)[i] == dims[i]
+
+def test_getdimonly():
+
+    filename = r'testdata/2x2_SNAP_CH=2_Z=5_T=2.czi'
+
+    sizes_czi = bf.czitools.read_dimensions_czi(filename)
+
+    dims = [1, 1, 2, 2, 5, 1216, 1216, 1]
+    dimorder = 'BSTCZYX0'
+
+    for i in range(0, len(dims)):
+        assert sizes_czi[0][i] == dims[i]
+
+    assert sizes_czi[1] == dimorder
 
 
 if __name__ == '__main__':
