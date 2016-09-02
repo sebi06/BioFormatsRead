b031b7424c1c770949daddd290ba37d1357e0922
sebi06 <sebi06@users.noreply.github.com>
2016-09-02 15:36:31 +0200

    Add files via upload
    Additional test tool.

---
+++ redirect.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Sebi
+
+File: redirect.py
+Date: 01.09.2016
+Version. 0.1
+"""
+
+import bfimage as bf
+import os
+
+script2test = 'test_get_image6d.py'
+
+of = bf.output2file(scriptname=script2test, output_name=script2test[:-3] + '_results.txt', targetdir=os.getcwd())
+
