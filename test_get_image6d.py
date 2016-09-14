97821ad8cd645fefc3ad76548c27be8eab03b6ce
sebi06 <sebi06@users.noreply.github.com>
2016-09-14 14:25:14 +0200

    Update test_get_image6d.py
    Updates.

--- test_get_image6d.py
+++ test_get_image6d.py
@@ -3,8 +3,8 @@
 @author: Sebi
 
 File: test_get_image6d.py
-Date: 03.02.2016
-Version. 1.0
+Date: 14.09.2016
+Version. 1.1
 """
 
 import numpy as np
@@ -12,7 +12,16 @@ import os
 import bfimage as bf
 import sys
 
-redirect = True
+#filename = r'testdata/Beads_63X_NA1.35_xy=0.042_z=0.1.czi'
+#filename = r'testdata/T=5_Z=3_CH=2_CZT_All_CH_per_Slice.czi'
+filename = r'testdata/B4_B5_S=8_4Pos_perWell_T=2_Z=1_CH=1.czi'
+
+# use for BioFormtas <= 5.1.10
+urlnamespace = 'http://www.openmicroscopy.org/Schemas/OME/2015-01'
+# use for BioFormtas > 5.2.0
+#urlnamespace = 'http://www.openmicroscopy.org/Schemas/OME/2016-06'
+
+redirect = False
 
 if redirect:
     # redirect output
@@ -21,23 +30,23 @@ if redirect:
     f = file(filepath_output, 'w')
     sys.stdout = f
 
-#filename = r'testdata/Beads_63X_NA1.35_xy=0.042_z=0.1.czi'
-#filename = r'testdata/T=5_Z=3_CH=2_CZT_All_CH_per_Slice.czi'
-filename = r'testdata/B4_B5_S=8_4Pos_perWell_T=2_Z=1_CH=1.czi'
-
 # specify bioformats_package.jar to use if required
 #bf.set_bfpath(insert path to bioformats_package.jar here)
 
 # get image meta-information
-MetaInfo = bf.bftools.get_relevant_metainfo_wrapper(filename)
+MetaInfo = bf.bftools.get_relevant_metainfo_wrapper(filename, ns=urlnamespace)
 img6d = bf.bftools.get_image6d(filename, MetaInfo['Sizes'])
 
 # show relevant image Meta-Information
 print '\n'
+print 'Testscript used      :  test_get_image6d.py'
+print 'OME NameSpace used   : ', urlnamespace
+print 'BF Version used      : ', bfpackage
+print '-------------------------------------------------------------'
 print 'Image Directory      : ', MetaInfo['Directory']
 print 'Image Filename       : ', MetaInfo['Filename']
 print 'Images Dim Sizes     : ', MetaInfo['Sizes']
-print 'Dimension Order*     : ', MetaInfo['DimOrder BF']
+print 'Dimension Order BF   : ', MetaInfo['DimOrder BF']
 print 'Dimension Order CZI  : ', MetaInfo['OrderCZI']
 print 'Shape CZI            : ', MetaInfo['ShapeCZI']
 print 'Total Series Number  : ', MetaInfo['TotalSeries']
@@ -51,8 +60,10 @@ print 'Em. Wavelengths [nm] : ', MetaInfo['WLEm']
 print 'Dyes                 : ', MetaInfo['Dyes']
 print 'Detector Model       : ', MetaInfo['Detector Model']
 print 'Detector Name        : ', MetaInfo['Detector Name']
+print 'Detector ID          : ', MetaInfo['DetectorID']
 print 'Channels             : ', MetaInfo['Channels']
 print 'Channel Description  : ', MetaInfo['ChDesc']
+print 'ImageIDs             : ', MetaInfo['ImageIDs']
 print 'Array Shape 6D       : ', np.shape(img6d)
 
 if redirect:
