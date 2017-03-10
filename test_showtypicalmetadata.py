b6ab3dfcf7ae0fd7a5e0fa1a3e45d4d4ac5fa403
sebi06 <sebi06@users.noreply.github.com>
2017-03-10 14:23:44 +0100

    Update test_showtypicalmetadata.py
    Updates.

--- test_showtypicalmetadata.py
+++ test_showtypicalmetadata.py
@@ -13,16 +13,18 @@ import bfimage as bf
 import sys
 
 # define testdata base directory and filename of the image dataset to be read
-filename = r'l:\Data\BioFormats_CZI_Test\CZI_Test\CZI_Test_20x20Tile.czi'
+#filename = r'testdata/Beads_63X_NA1.35_xy=0.042_z=0.1.czi'
+#filename = r'testdata/T=5_Z=3_CH=2_CZT_All_CH_per_Slice.czi'
+filename = r'testdata/B4_B5_S=8_4Pos_perWell_T=2_Z=1_CH=1.czi'
 
 # specify bioformats_package.jar to use if required
-bfpackage = r'c:\Users\M1SRH\Documents\Software\BioFormats_Package\5.3.2\bioformats_package.jar'
-#bf.set_bfpath(bfpackage)
+bfpackage = r'BioFormats/5.1.10/bioformats_package.jar'
+bf.set_bfpath(bfpackage)
 
 # use for BioFormtas <= 5.1.10
-#urlnamespace = 'http://www.openmicroscopy.org/Schemas/OME/2015-01'
+urlnamespace = 'http://www.openmicroscopy.org/Schemas/OME/2015-01'
 # use for BioFormtas > 5.2.0
-urlnamespace = 'http://www.openmicroscopy.org/Schemas/OME/2016-06'
+#urlnamespace = 'http://www.openmicroscopy.org/Schemas/OME/2016-06'
 
 redirect = True
 
