5a9459a495c2f11c2eb430eca07c276851fe36e7
sebi06 <sebi06@users.noreply.github.com>
2017-03-10 13:55:03 +0100

    Update showZwellplate.py
    Updates.

--- showZwellplate.py
+++ showZwellplate.py
@@ -12,25 +12,26 @@ import dispZsurface as dsp
 import pandas as pd
 import matplotlib.pyplot as plt
 
-#filenameczi = r's:\Python_Projects_Testdata\CZI\testwell96.czi'
-filenameczi = r'/home/sebi06/Dokumente/Python_Projects_Testdata/CZI/testwell96.czi'
 #filenamecsv = r'testdata/Wellchamber_384_Comb.csv'
 #filenamecsv = r'testdata/fixed endpoint 3C 2_5 384well_planetable.csv'
-#filenamecsv = r'testdata/testwell96_planetable.csv'
-filenamecsv = r'/home/sebi06/Dokumente/Python_Projects_Testdata/CZI/testwell96_planetable.csv'
+#filenameczi = r'testadata/yourCZIimage.czi'
+filenamecsv = r'testdata/testwell96_planetable.csv'
+
+# define separator
+separator = '\t'
 
 # create plane info from CZI image file and write CSV file (optional)
-#planetable1, filenamecsv = bf.get_planetable(filenameczi, writecsv=True, separator='\t')
+#planetable, filenamecsv = bf.get_planetable(filenameczi, writecsv=True, sep=separator)
 
 # or use the CSV file directly once it was created
-planetable2 = pd.read_csv(filenamecsv, sep='\t')
+planetable = pd.read_csv(filenamecsv, sep=separator)
 
 # show the dataframe
-print planetable2[:10]
-print planetable2.shape[0]
+print planetable[:10]
+print planetable.shape[0]
 
 # display the XYZ positions
-dsp.scatterplot(planetable2, ImageID=0, T=0, CH=0, Z=0, size=250,
+dsp.scatterplot(planetable, ImageID=0, T=0, CH=0, Z=0, size=250,
                 savefigure=False, figsavename=filenameczi, showsurface=True)
 
 # show the plot
