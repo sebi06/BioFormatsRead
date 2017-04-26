b9b32cff06d0171e4495f7e387d7e67c085c5b0c
sebi06 <sebrhode@gmail.com>
2017-04-26 21:49:24 +0200

    Updates.

--- bfimage/setup.py
+++
@@ -1,5 +0,0 @@
-# setup.py
-# Usage: ``python setup.py build_ext --inplace``
-from distutils.core import setup, Extension
-import numpy
-setup(name='_tifffile', ext_modules=[Extension('_tifffile', ['tifffile.c'], include_dirs=[numpy.get_include()])])
