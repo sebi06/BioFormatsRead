e8ff9f4d6e435bdcdd371d908dee406bf1697807
sebi06 <sebi06@users.noreply.github.com>
2017-05-12 10:34:58 +0200

    Update test_getdimonly.py

--- test_getdimonly.py
+++ test_getdimonly.py
@@ -15,7 +15,7 @@ import czitools as czt
 filenames = [r'testdata/2x2_SNAP_CH=2_Z=5_T=2.czi']
 
 # specify bioformats_package.jar to use if required
-bfpackage = r'BioFormats/5.4.1/bioformats_package.jar'
+bfpackage = r'bfpackage/5.4.1/bioformats_package.jar'
 bf.set_bfpath(bfpackage)
 
 for currentfile in filenames:
