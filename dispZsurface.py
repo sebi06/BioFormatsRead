be0298200d141526f89a79d30e095208f58d52cf
sebi06 <sebi06@users.noreply.github.com>
2017-05-30 13:53:19 +0200

    Update dispZsurface.py

--- dispZsurface.py
+++ dispZsurface.py
@@ -76,28 +76,17 @@ def scatterplot(planetable, ImageID=0, T=0, Z=0, CH=0, size=35,
     # normalize z-data by substracting the minimum value
     zpos_norm = zpos - zpos.min()
 
-    # delta xy for plotting the coordinate system in [micron]
-    delta = 3000
-    if xpos.min() - delta < 0:
-        xmin = 0
-    else:
-        xmin = xpos.min()
-    if ypos.min() - delta < 0:
-        ymin = 0
-    else:
-        ymin = ypos.min()
-
-    fig1 = plt.figure(figsize=(10, 6), dpi=100)
+    #fig1 = plt.figure(figsize=(10, 6), dpi=100)
+    fig1 = plt.figure()
     ax1 = fig1.add_subplot(111)
     ax1.grid(True)
     plt.axis('equal')
 
-    # set the plot limits
-    ax1.set_xlim(0, xpos.max() + delta)
-    ax1.set_ylim(0, ypos.max() + delta)
-
     # invert the Y-axis --> O,O = Top-Left
     ax1.invert_yaxis()
+
+    ax1.autoscale(enable=True, axis='x', tight=True)
+    ax1.autoscale(enable=True, axis='y', tight=True)
 
     # define the labels
     ax1.set_title('XYZ-Positions (norm) : ' + 'ImageID=' + str(ImageID) + ' T=' + str(T) + ' Z='+ str(Z) + ' CH=' + str(CH))
@@ -106,7 +95,7 @@ def scatterplot(planetable, ImageID=0, T=0, Z=0, CH=0, size=35,
 
     # plot data and label the colorbar
     sc1 = plt.scatter(xpos, ypos, marker='s', c=zpos_norm, s=size, cmap=cm.coolwarm)
-    cb1 = plt.colorbar(sc1)
+    cb1 = plt.colorbar(sc1, fraction=0.046, shrink=0.8, pad=0.04)
     cb1.set_label('Z-Offset [micron]', labelpad=20)
 
     # optional save figure as PNG
@@ -120,12 +109,12 @@ def scatterplot(planetable, ImageID=0, T=0, Z=0, CH=0, size=35,
         fig2 = plt.figure(figsize=(10, 6), dpi=100)
         ax2 = fig2.add_subplot(111, projection='3d')
 
-        # set the plot limits
-        ax2.set_xlim(0, xpos.max() + delta)
-        ax2.set_ylim(0, ypos.max() + delta)
-
         # invert the Y-axis --> O,O = Top-Left
         ax2.invert_yaxis()
+
+        ax1.autoscale(enable=True, axis='x', tight=True)
+        ax1.autoscale(enable=True, axis='y', tight=True)
+        ax1.autoscale(enable=True, axis='z', tight=True)
 
         # define the labels
         ax2.set_xlabel('Stage X-Axis [micron]')
@@ -135,7 +124,7 @@ def scatterplot(planetable, ImageID=0, T=0, Z=0, CH=0, size=35,
         # plot data and label the colorbar
         #sc2 = ax2.plot(xpos, ypos, zpos_norm, '.', markersize=10, cmap=plt.cm.coolwarm)
         sc2 = ax2.scatter(xpos, ypos, zpos_norm, marker='.', s=200, c=zpos_norm, cmap=plt.cm.coolwarm, depthshade=False)
-        cb2 = plt.colorbar(sc2, shrink=.75)
+        cb2 = plt.colorbar(sc2, shrink=0.8)
         cb2.set_label('Z-Offset [micron]', labelpad=20)
 
     if not showsurface:
