657b5bff580ec1a2f4fcffcd8db736287dbd5415
sebi06 <sebi06@users.noreply.github.com>
2017-05-30 11:37:23 +0200

    Update showZwellplate.py

--- showZwellplate.py
+++ showZwellplate.py
@@ -3,19 +3,22 @@
 @author: Sebi
 
 File: showZwellplate.py
-Date: 17.04.2017
-Version. 1.2
+Date: 30.05.2017
+Version. 1.3
 """
 
 from __future__ import print_function
 import bftools as bf
 import dispZsurface as dsp
-#import pandas as pd
 import matplotlib.pyplot as plt
+import os
+
+saveformat = '.png'
 
 #filenamecsv = r'testdata/Wellchamber_384_Comb.csv'
 #filenamecsv = r'testdata/fixed endpoint 3C 2_5 384well_planetable.csv'
-filenameczi = r'testdata/B4_B5_S=8_4Pos_perWell_T=2_Z=1_CH=1.czi'
+#filenameczi = r'testdata/B4_B5_S=8_4Pos_perWell_T=2_Z=1_CH=1.czi'
+filenameczi = r'testdata/testwell96.czi'
 #filenamecsv = r'testdata/testwell96_planetable.csv'
 
 # use for BioFormtas <= 5.1.10
@@ -24,7 +27,7 @@ filenameczi = r'testdata/B4_B5_S=8_4Pos_perWell_T=2_Z=1_CH=1.czi'
 urlnamespace = 'http://www.openmicroscopy.org/Schemas/OME/2016-06'
 
 # specify bioformats_package.jar to use if required
-bfpackage = r'bfpackage/5.4.1/bioformats_package.jar'
+bfpackage = r'bfpackage/5.5.0/bioformats_package.jar'
 bf.set_bfpath(bfpackage)
 
 # define separator
@@ -40,9 +43,12 @@ planetable, filenamecsv = bf.get_planetable(filenameczi, writecsv=True, separato
 print(planetable[:10])
 print(planetable.shape[0])
 
+# define name for figure to be saved
+figuresavename = os.path.splitext(filenamecsv)[0] + '_XYZ-Pos' + saveformat
+
 # display the XYZ positions
-dsp.scatterplot(planetable, ImageID=0, T=0, CH=0, Z=0, size=250,
-                savefigure=False, figsavename=filenamecsv, showsurface=True)
+fig1, fig2 = dsp.scatterplot(planetable, ImageID=0, T=0, CH=0, Z=0, size=250,
+                savefigure=True, figsavename=figuresavename, showsurface=True)
 
 # show the plot
 plt.show()
